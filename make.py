a0154795e724ea3c6d82c3e14b4ef74c98841b47
Steve Dower <steve.dower@python.org>
2025-01-01 11:11:25 +0000

    Grab current tag when building

--- make.py
+++ make.py
@@ -1,3 +1,5 @@
+import os
+import subprocess
 import sys
 from subprocess import check_call as run
 from _make_helper import get_dirs, rmtree, unlink
@@ -14,8 +16,22 @@ if "-i" not in sys.argv:
     rmtree(TEMP)
     rmtree(LAYOUT)
 
+ref = "none"
+try:
+    with subprocess.Popen(
+        ["git", "describe", "HEAD", "--tags"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    ) as p:
+        out, err = p.communicate()
+    if out:
+        ref = "refs/tags/" + out.decode().strip()
+    print("Building for tag", ref)
+except subprocess.CalledProcessError:
+    pass
+
 # Run main build - this fills in BUILD and LAYOUT
-run([sys.executable, "-m", "pymsbuild"], cwd=DIRS["root"])
+run([sys.executable, "-m", "pymsbuild"], cwd=DIRS["root"], env={**os.environ, "GITHUB_REF": ref})
 
 # Overwrite bundled feed. This will be removed eventually
 run([sys.executable, "scripts/generate-nuget-index.py", LAYOUT / "bundled" / "index.json"])
